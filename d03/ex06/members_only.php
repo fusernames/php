a777b16c415016e8e0627eaadd6a84376c87b2af
Alexis CAROFF <alcaroff@e2r8p2.42.fr>
2018-11-09 10:59:51 +0100

    ex06

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -1,9 +1,18 @@
 <?php
-if (!isset($_SERVER['PHP_AUTH_USER']) {
-	header('WWW-Authenticate: Basic realm="AH"');
+$usr = 'zaz';
+$pwd = 'jaimelespetitsponeys';
+
+if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || ($_SERVER['PHP_AUTH_USER'] != $usr) || ($_SERVER['PHP_AUTH_PW'] != $pwd)) {
+	header('WWW-Authenticate: Basic realm="My Realm"');
 	header('HTTP/1.0 401 Unauthorized');
+	echo 'Text to send if user hits Cancel button';
+	exit;
+} else {
+	$data = file_get_contents('../img/42.png');
+	$img = 'data:image/png;base64,'.base64_encode($data);
 }
-else {
-	echo 'salut';
-	echo $_SERVER['PHP_AUTH_USER'];
-}
+?>
+<html><body>
+Bonjour Zaz<br />
+<img src='<?php echo $img ?>'>
+</body></html>
