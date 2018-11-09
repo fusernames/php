a372489ed6da8c4d725fac85dd8fa845bdfc57af
Alexis CAROFF <alcaroff@e2r8p2.42.fr>
2018-11-09 12:51:53 +0100

    d03/ex06 done

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -2,17 +2,17 @@
 $usr = 'zaz';
 $pwd = 'jaimelespetitsponeys';
 
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || ($_SERVER['PHP_AUTH_USER'] != $usr) || ($_SERVER['PHP_AUTH_PW'] != $pwd)) {
+if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
 	header('WWW-Authenticate: Basic realm="My Realm"');
 	header('HTTP/1.0 401 Unauthorized');
-	echo 'Text to send if user hits Cancel button';
-	exit;
-} else {
-	$data = file_get_contents('../img/42.png');
-	$img = 'data:image/png;base64,'.base64_encode($data);
-}
-?>
+exit;
+$data = file_get_contents('../img/42.png');
+$img = 'data:image/png;base64,'.base64_encode($data);
+if (($_SERVER['PHP_AUTH_USER'] == $usr) && ($_SERVER['PHP_AUTH_PW'] == $pwd)): ?>
 <html><body>
-Bonjour Zaz<br />
-<img src='<?php echo $img ?>'>
+	Bonjour Zaz<br />
+	<img src='<?php echo $img ?>'>
 </body></html>
+<?php else: ?>
+<body><html>Cette zone est accessible uniquement aux membres du site</body></html>
+<?php endif; ?>
