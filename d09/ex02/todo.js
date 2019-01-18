ab86d446dcd8ac10be556a3c286484e9e9208c66
Alexis CAROFF <alcaroff@e1r9p18.42.fr>
2019-01-18 21:55:46 +0100

    jquery

---
+++ d09/ex02/todo.js
@@ -0,0 +1,54 @@
+var button = document.getElementById('new');
+var lst = document.getElementById('ft_list');
+loadCookie();
+
+button.onclick = function () {addTodo(prompt('Enter new thing to do'))};
+
+function removeChild(elem) {
+  elem.display = 'none';
+  if (confirm('Do you really want to delete this element?')) {
+    lst.removeChild(elem);
+    createCookie();
+  }
+}
+
+function createCookie() {
+  var elem = lst.firstChild;
+  var str = '';
+  while((elem) !== null) {
+    if (typeof elem.innerHTML !== 'undefined')
+      str = elem.innerHTML + '\\' + str;
+    elem = elem.nextSibling
+  }
+  document.cookie = 'todo=' + str + '; expires=Fri, 31 Dec 9999 23:59:59 GMT path=/;';
+}
+
+function addTodo(todo) {
+  if (todo === null)
+    return false;
+  var div = document.createElement('div');
+  var id = parseInt(lst.firstChild.id);
+  div.innerHTML = todo;
+  div.onclick = function() {
+    removeChild(div);
+  };
+  if (!isNaN(id)) {
+    div.id = id + 1;
+  } else {
+    div.id = 1;
+  }
+  lst.insertBefore(div, lst.firstChild);
+  createCookie(todo);
+}
+
+function loadCookie() {
+  if (document.cookie.includes('todo=')) {
+    var str = document.cookie.split(';');
+    var cookie = str[0].split('=');
+    if (cookie[0] == 'todo') {
+      var todos = cookie[1].split('\\');
+      for (i = 0; todos[i]; i++)
+      addTodo(todos[i]);
+    }
+  }
+}
